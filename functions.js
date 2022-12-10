da676c93d6ecb00dbaa403c44f65d311348d252c
William Lavery <lavery_william@yahoo.com>
2022-12-10 15:53:18 -0500

    Initial commit

--- functions.js
+++ functions.js
@@ -1,11 +1,32 @@
 // 1: Write a function named printGreeting that prints a simple greeting message, the same one, every time it is called. Call this function 3 times.
-
+function printGreeting() {
+    console.log("Hi, how are you?");
+}
+printGreeting();
+printGreeting();
+printGreeting();
 
 // 2: Write a function that accepts 1 argument. The function should console.log a sentence that interpolates the data passed in.
+function printGreeting(name) {
+    console.log(`Hi, how are you ${name}?`);
+}
+printGreeting("Tito");
 
+// 3: Write a function that has 3 parameters: a string and two numbers. The String will be the name of a company, and the numbers will 
+// represent the minimum and maximum of a pay range for a posted job. The function should print out a sentence that includes the name of the 
+// company and the range itself (if the numbers passed in are 90000 and 110000, the pay range is 20000).
+function jobListing(maxSalary, minSalary) {
+    var sum = maxSalary - minSalary;
+    return sum;
+}
 
-// 3: Write a function that has 3 parameters: a string and two numbers. The String will be the name of a company, and the numbers will represent the minimum and maximum of a pay range for a posted job. The function should print out a sentence that includes the name of the company and the range itself (if the numbers passed in are 90000 and 110000, the pay range is 20000).
+var maxSalary = 100000;
+var minSalary = 80000;
+var salaryRange = (maxSalary - minSalary);
 
+console.log(`Initech offers job with salary range of ${salaryRange}`);
+
+jobListing(100000, 80000);
 
 // 4: Write a function that satifies the following interaction pattern:
 
